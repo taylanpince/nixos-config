8c865c95253907a51914dbe4dca99c161dfc64af
Taylan Pince <taylanpince@gmail.com>
2026-01-25 17:48:14 +0100

    Hardware monitoring and management tools

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -297,12 +297,19 @@
     mpv
     ffmpeg-full
     celluloid
+
+    # Hardware
+    btop
+    power-profiles-daemon
+    libva-utils
   ];
 
   fonts.packages = with pkgs; [
     inter
     nerd-fonts.jetbrains-mono
   ];
+
+  services.power-profiles-daemon.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
