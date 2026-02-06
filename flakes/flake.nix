c83adfa001e2cb0639dc417f58a63c5c386bb72a
Taylan Pince <taylanpince@gmail.com>
2026-02-07 00:52:32 +0100

    Add go flakes for dev

---
+++ flakes/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "dev flakes";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system} = {
+        go = pkgs.mkShell {
+          packages = with pkgs; [
+            go_1_23
+            gopls
+            delve
+            golangci-lint
+          ];
+        };
+
+        go-cgo = pkgs.mkShell {
+          packages = with pkgs; [
+            go_1_23
+            gopls
+            delve
+            golangci-lint
+            gcc
+            pkg-config
+            openssl
+            zlib
+            sqlite
+          ];
+        };
+      };
+    };
+}
+
