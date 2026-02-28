36e0d6918ef27c2507d7883afc810f101d029e91
Taylan Pince <taylanpince@gmail.com>
2026-02-28 19:12:59 +0100

    feat: add home-manager scaffold (#3)

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -20,6 +25,13 @@
         inherit system;
         modules = [
           ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.taylan = import ./home/taylan.nix;
+          }
         ];
       };
 
