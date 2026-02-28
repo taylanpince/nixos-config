1915110c84dc9902f5ec581019430d36d4657466
Taylan Pince <taylanpince@gmail.com>
2026-02-28 19:02:55 +0100

    feat: add home-manager scaffold

---
+++ nixos/home/taylan.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "taylan";
+  home.homeDirectory = "/home/taylan";
+
+  # Required by home-manager
+  home.stateVersion = "25.11";
+
+  # Keep this minimal to start. We can migrate your dotfiles/configs here gradually.
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Taylan Pince";
+    userEmail = "taylan@users.noreply.github.com";
+  };
+
+  programs.starship.enable = true;
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  # A few CLI niceties (matches what you already install system-wide)
+  programs.fzf.enable = true;
+  programs.zoxide.enable = true;
+  programs.eza.enable = true;
+  programs.bat.enable = true;
+
+  # Let home-manager manage user packages as you decide. Starting empty.
+  home.packages = [ ];
+}
