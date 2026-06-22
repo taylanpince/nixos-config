059898ae321869d24104186a1776e69e9d239630
Taylan Pince <taylanpince@gmail.com>
2026-06-22 13:56:38 +0200

    Upgrade wrangler to latest

--- nixos/shells/node.nix
+++ nixos/shells/node.nix
@@ -1,4 +1,20 @@
 { pkgs }:
+
+let
+  wrangler = pkgs.wrangler.overrideAttrs (old: rec {
+    version = "4.103.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "cloudflare";
+      repo = "workers-sdk";
+      rev = "wrangler@${version}";
+      hash = "sha256-u2phFQCUmmkrKoy05MHRA3rBUXwpKFlVRHI/qi3Uojg=";
+    };
+    pnpmDeps = old.pnpmDeps.overrideAttrs (_: {
+      inherit src;
+      outputHash = "sha256-MaqocUoaHDR+orOOwPy3ta3AzzwmguebRlcMO4WCHHU=";
+    });
+  });
+in
 {
   node = pkgs.mkShell {
     name = "node";
